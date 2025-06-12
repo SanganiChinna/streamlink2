e912297fa7718460e5603208a1759d3cb9a0b670
Kenshin <sanganidurgarao53@gmail.com>
2025-06-12 13:36:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,15 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // typescript: { // Removed: Better to ensure no build errors.
-  //   ignoreBuildErrors: true,
-  // },
-  // eslint: { // Removed: Better to ensure no lint errors during build.
-  //   ignoreDuringBuilds: true,
-  // },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'drive.google.com',
         port: '',
-        pathname: '/**', // Added from next.config.js
+        pathname: '/**',
       },
       {
         protocol: 'https',
